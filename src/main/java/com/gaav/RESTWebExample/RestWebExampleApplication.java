7bb6369ab6c56bc7ff8c48f434e9dd6ac07f3005
tecnologia-9 <tecnologia9@tecnologia9.local>
2022-10-11 17:22:18 -0500

    first commit

---
+++ src/main/java/com/gaav/RESTWebExample/RestWebExampleApplication.java
@@ -0,0 +1,13 @@
+package com.gaav.RESTWebExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestWebExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestWebExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gaav/RESTWebExample/RestWebExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gaav.RESTWebExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWebExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
